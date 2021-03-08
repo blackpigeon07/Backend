200392a36138ea19eac86e01c9d8ce67737c6ec0
AmanPandey0320 <72158086+AmanPandey0320@users.noreply.github.com>
2021-03-08 20:25:18 +0530

    storage routes

--- api/api.router.js
+++ api/api.router.js
@@ -2,5 +2,6 @@ const {Router} = require('express');
 const router = Router();
 
 router.use('/auth',require('./auth/auth.router'));
+router.use('/storage',require('./storage/storage.router'));
 
 module.exports = router;
